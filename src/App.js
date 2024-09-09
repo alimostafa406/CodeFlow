47ed198b82a4dcc80c4f4fdd417692467ed8788e
alimostafa <alimostafa406@gmail.com>
2024-09-09 20:35:38 +0300

    technology

--- src/App.js
+++ src/App.js
@@ -3,30 +3,37 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './pages/Header/Layout';
 import Header from './pages/Header/Header';
 import Section from './pages/section/Section';
-import Services from './pages/Services/Services';  // Only one import for Services
-import Technologies  from './pages/technology/technologies';  // Only one import for Services
+import Services from './pages/Services/Services';
 import About from './pages/About/About';
 import Contact from './pages/Contact/Contact';
 import Home from './pages/Home';
+import { useState } from 'react';
+import Technologies from './pages/technology/technologies';
+
 
 function App() {
-  return (
-    <>
-      {/* <BrowserRouter>
-        <Routes>
-          <Route path="" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="/" element={<Header />} />
-            <Route path="about" element={<About />} />
-            <Route path="section" element={<Section />} />
-            <Route path="services" element={<Services />} />
-            <Route path="contact" element={<Contact />} />
-          </Route>
-        </Routes>
-      </BrowserRouter> */}
-      <Technologies />  
-    </>
-  );
+
+  const [open, setOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setOpen(!open);
+  };
+
+  return(
+    <BrowserRouter>
+    <Routes>
+      <Route path="" element={<Layout/>}>
+        <Route index element={<Home/>}/>
+        <Route path='/' element={<Header/>}/>
+        <Route path="about" element={<About/>}/>
+        <Route path="section" element={<Section/>}/>
+        <Route path="services" element={<Services/>}/>
+        <Route path="contact" element={<Contact/>}/>
+        <Route path="technology" element={<Technologies/>}/>
+      </Route>
+    </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
